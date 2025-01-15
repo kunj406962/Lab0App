aa75e23e99636fc5fdb05f2de851e2de01f39b81
Kunj Patel <pkunjk007@gmail.com>
2025-01-15 15:12:05 -0700

    Add project files.

---
+++ Lab0/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * CPRG 211 Lab 0 Activity
+ * Author: Kunj
+ * When: Winter 2025
+ */
+
+// get low and high int numbers
+// low must be positive
+// high must be greater than low
+
+using Lab0;
+
+int low, high;
+int diff;   // difference between low and high
+string[] readText;
+int total=0;
+int num;
+bool primenum;
+
+low = Utilities.GetPositiveInt("low number");
+
+high = Utilities.GetIntInRange("high number", low + 1, Int32.MaxValue); // the largest int possible
+
+
+// calculate and  print the difference
+diff = high - low;
+Console.WriteLine($"The difference between {low} and {high} is {diff}");
+
+// create an array to hold numbers between low and high
+int[] numbers = new int[diff + 1]; // size of the array
+for(int i = 0; i <= diff; i++) // i is the index
+{
+    numbers[i] = low + i;
+}
+
+Console.WriteLine("Numbers in the array:");
+for (int i = 0; i <= diff; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+// create a file named "number.txt" and write to it the numbers from the array in reverse order
+StreamWriter streamWriter = File.CreateText("numbers.txt"); // located in the same folder as the .exe file
+for(int i = numbers.Length - 1; i >= 0; i--)
+{
+    streamWriter.WriteLine(numbers[i]);
+}
+streamWriter.Close();// important to finalize the writing
+Console.WriteLine("File written");
+
+// read "number.txt", get the numbers aand add them
+readText = File.ReadAllLines("numbers.txt"); // opening the fie
+for (int i = 0; i < readText.Length; i++)
+{
+    num= Convert.ToInt32(readText[i]); //converting the lines in string format to integers
+    total += num;
+}
+Console.WriteLine($"\nThe total of the numbers is {total}");
+
+//print prime numbers
+Console.WriteLine($"\nThe Prime numbers between {low} and {high} are:");
+for (int i = 0; i < numbers.Length; i++)
+{
+    primenum = Utilities.PrimeNumber(numbers[i]);
+    if (primenum)
+    {
+        Console.WriteLine(numbers[i]);
+    }
+
+}
+
+
---
+++ Lab0/Utilities.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab0
+{
+    /// <summary>
+    /// Methods for getting and validating input data and relevant calculation
+    /// </summary>
+    internal static class Utilities
+    {
+        /// <summary>
+        /// reads positive int
+        /// </summary>
+        /// <param name="prompt"> name used in error messages</param>
+        /// <returns>int value read that is > 0</returns>
+        public static int GetPositiveInt(string prompt)
+        {
+            bool gotIt = false;
+            int result = 0;
+            string input;
+
+            while (!gotIt) 
+            {
+                Console.WriteLine($"Enter {prompt}:"); 
+                input = Console.ReadLine() ?? ""; 
+
+                if (Int32.TryParse(input, out result)) // result is assigned the parsed value
+                {
+                    if (result > 0)
+                    {
+                        gotIt = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {prompt} must be positive");
+                    }
+                }
+                else // could not parse - not a int
+                {
+                    Console.WriteLine($"Error: {prompt} must be a whole number");
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// reads int in range
+        /// </summary>
+        /// <param name="prompt">name used in error messages</param>
+        /// <param name="minValue">minimum value for the range</param>
+        /// <param name="maxValue">maximum value for the range</param>
+        /// <returns>int value read that is within range</returns>
+        public static int GetIntInRange(string prompt, int minValue, int maxValue)
+        {
+            bool gotIt = false;
+            int result = 0;
+            string input;
+
+            while (!gotIt)
+            {
+                Console.WriteLine($"Enter {prompt}:");
+                input = Console.ReadLine() ?? "";
+
+                if (Int32.TryParse(input, out result)) // result is assigned the parsed value
+                {
+                    if (result >= minValue && result <= maxValue)
+                    {
+                        gotIt = true;
+                    }
+                    else
+                    {
+                        if(minValue == Int32.MinValue)
+                            Console.WriteLine($"Error: {prompt} must be less than or equal to {maxValue}");
+                        else if(maxValue == Int32.MaxValue)
+                            Console.WriteLine($"Error: {prompt} must be greater than or equal to {minValue}");
+                        else
+                            Console.WriteLine($"Error: {prompt} must be within range {minValue} - {maxValue}");
+                    }
+                }
+                else // could not parse - not a int
+                {
+                    Console.WriteLine($"Error: {prompt} must be a whole number");
+                }
+            }
+            return result;
+        }
+
+        public static bool PrimeNumber(int number) 
+        {
+            bool result=false;
+            if (number == 2 || number==3)
+            {
+                result = true;
+            }
+            else if (number % 2 != 0 && number %3 != 0)
+            {
+                result = true;
+
+            }
+            
+            return (result);
+        }
+
+    }//class
+}// namespace
